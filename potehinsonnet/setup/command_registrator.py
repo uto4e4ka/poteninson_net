ca74e4ce6bba78567cc6d5967fa4bd80637c8b4f
Uto4ka <alezhestkov@gmail.com>
2026-09-09 22:18:32 +0400

    add responses

--- potehinsonnet/setup/command_registrator.py
+++ potehinsonnet/setup/command_registrator.py
@@ -14,20 +14,22 @@ class CommandRegistrator:
         self.subs = {}
 
     async def register_command(self,command:Command, listener: Callable[[ExecutedCommand], Awaitable[ExecutedCommandResponse]]):
+        if f"{command.service}.{command.tag}" in self.subs.keys():
+            raise KeyError("tag must be unique")
         await self.nats_client.publish("discord.command.register",command.model_dump(mode='json'))
         async def on_call(body)->dict:
             body = ExecutedCommand.model_validate(body)
             result = await listener(body)
             return result.model_dump(mode='json')
 
-        self.subs[f"{command.service}.{command.name}"] = await self.nats_client.subscribe(
+        self.subs[f"{command.service}.{command.tag}"] = await self.nats_client.subscribe(
             f"discord.command.execute.{command.service}.{command.tag}",
             on_call)
 
 
     async def unregister_command(self,command:Command):
         await self.nats_client.publish("discord.command.remove",command.model_dump(mode='json'))
-        key = f"{command.service}.{command.name}"
+        key = f"{command.service}.{command.tag}"
         sub = self.subs.pop(key,None)
         if sub:
             sub.unsubscribe()
