9f83b294b52ef40cc209b3a6ff498e2b64477bd7
Uto4ka <alezhestkov@gmail.com>
2026-09-15 10:27:08 +0400

    containers upodate

--- potehinsonnet/containers/nats_container.py
+++ potehinsonnet/containers/nats_container.py
@@ -11,9 +11,19 @@ from potehinsonnet.net_models.system_models import Service
 from potehinsonnet.setup.command_registrator import CommandRegistrator
 
 
+@asynccontextmanager
+async def _init_health(client:NatsClient,plugin:Service)->AsyncGenerator[Health, None]:
+    health = Health(client=client,plugin=plugin)
+    await health.start()
+    yield health
+    await health.stop()
 
-
-
+@asynccontextmanager
+async def _init_nats(url:str)->AsyncGenerator[NatsClient, None]:
+    nats = NatsClient(url)
+    await nats.connect()
+    yield nats
+    await nats.close()
 
 class NatsContainer(containers.DeclarativeContainer):
     config: Configuration = providers.Configuration(
@@ -23,7 +33,7 @@ class NatsContainer(containers.DeclarativeContainer):
             },
         }
     )
-    nats: Resource[NatsClient] = providers.Resource(NatsClient,
+    nats: Resource[NatsClient] = providers.Resource(_init_nats,
                                                                 url = config.nats.url)
     plugin: Singleton[Service] = providers.Singleton(Service,
                                                     type= config.plugin.type,
@@ -35,7 +45,7 @@ class NatsContainer(containers.DeclarativeContainer):
                                                     icon = config.plugin.icon,
                                                     site = config.plugin.site
                                                     )
-    health: Resource[Health] = providers.Resource(Health,
+    health: Resource[Health] = providers.Resource(_init_health,
                                                   client = nats,
                                                   plugin = plugin,
                                                   )
